e3d25e8f6ae2ed7d808a6126a911d0f74f3a5a3a
anne-kmmr <annminecraft07@gmx.de>
2026-01-15 15:23:50 +0100

    helppppppp

--- Quotient.java
+++ Quotient.java
@@ -2,9 +2,9 @@
     public static void main(String args[]) {
         float a, b, quotient;
         java.util.Scanner scan = new java.util.Scanner(System.in);
-        System.out.println("Enter a: ");
+        System.out.print("Enter a: ");
         a = scan.nextFloat();
-        System.out.println("Enter b: ");
+        System.out.print("Enter b: ");
         b = scan.nextFloat();
         if (b != 0) {
             quotient = a / b;
