a758f9cd72943a6324d91f080f55f2a598a9c2c2
anne-kmmr <annminecraft07@gmx.de>
2026-01-19 11:17:00 +0100

    added files and doing some exercises

---
+++ de/dhbwka/java/exercise/datatypes/ShortValue.java
@@ -0,0 +1,17 @@
+package de.dhbwka.java.exercise.datatypes;
+
+public class ShortValue {
+
+    public static void main(String[] args) {
+
+        //nicht möglich, da short zu klein ist
+        // for(short i = 32767; i <= 32768; i++){
+        //     System.out.println(i);
+        // }
+
+        for(int i = 32767; i <= 32768; i++){
+            System.out.println(i);
+        } 
+    }
+    
+}
